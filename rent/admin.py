83c0cd87182f527c572e5d6a14b235e7ca59222d
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2021-02-11 21:20:50 +0300

    add settings for admin site

--- rent/admin.py
+++ rent/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from .models import Applications, Premises, PurposesOfPremises
 
-admin.site.register((Applications, Premises, PurposesOfPremises))
+
+class PremisesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'areaName', 'describe', 'area', 'floor', 'price')
+    list_display_links = ('id', 'areaName', 'describe')
+    search_fields = ('areaName', 'describe',)
+
+
+class ApplicationsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'premises', 'additionalInfo', 'created_at')
+    list_display_links = ('id', 'client', 'premises')
+    search_fields = ('additionalInfo', )
+
+
+admin.site.register(Applications, ApplicationsAdmin)
+admin.site.register(PurposesOfPremises)
+admin.site.register(Premises, PremisesAdmin)
--- rent/apps.py
+++ rent/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class RentConfig(AppConfig):
     name = 'rent'
+    verbose_name = 'Аренда'
