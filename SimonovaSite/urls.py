37f58da3a350e6728b1e6d2d48a4792b2ca53e9f
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2021-02-19 18:05:35 +0300

    add registration

--- SimonovaSite/urls.py
+++ SimonovaSite/urls.py
@@ -17,13 +17,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.auth.views import LoginView, LogoutView
+from rent.views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rent.urls')),
     path('accounts/login', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout')
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('signup/',  SignUpView.as_view(), name='signup')
 ]
 
 if settings.DEBUG:
--- rent/views.py
+++ rent/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from .forms import PremisesForm
 
 from .models import Premises
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
-
+# вывод списка всех офисов
 def index(request):
     premises = Premises.objects.all()
     context = {
@@ -14,8 +17,8 @@ def index(request):
     return render(request, template_name='rent/index.html', context=context)
 
 
+# добавление офиса
 def image_upload_view(request):
-
     if request.method == 'POST':
         form = PremisesForm(request.POST, request.FILES)
         if form.is_valid():
@@ -25,3 +28,9 @@ def image_upload_view(request):
         form = PremisesForm()
     return render(request, 'rent/createPremises.html', {'form': form})
 
+
+# регистрация
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
