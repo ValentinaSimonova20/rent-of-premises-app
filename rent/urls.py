cfc2ef6a9116254e56ed432fbc4231af43fe2a85
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2021-02-20 18:28:58 +0300

    add create application by user function

--- rent/urls.py
+++ rent/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from rent.views import index, image_upload_view, by_area
+from rent.views import index, image_upload_view, by_area, get_apps, add_app
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('areas/', index, name='areas'),
     path('add/', image_upload_view, name='add'),
-    path('<int:area_id>/', by_area, name='by_area')
+    path('<int:area_id>/', by_area, name='by_area'),
+    path('apps/', get_apps, name='apps'),
+    path('<int:area_id>/addApp', add_app, name='addApp')
 
 ]
--- rent/views.py
+++ rent/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import PremisesForm
 
-from .models import Premises
+from .models import Premises, Applications
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
@@ -42,3 +42,27 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+# отображение заявок пользователя
+def get_apps(request):
+    if request.user.is_staff:
+        applications = Applications.objects.all()
+    else:
+
+        applications = Applications.objects.all().filter(client=request.user)
+    context = {
+        'apps': applications
+    }
+
+    return render(request, template_name='rent/applications.html', context=context)
+
+
+# подать заявку на площадь
+def add_app(request, area_id):
+    current_area = Premises.objects.get(pk=area_id)
+    new_app = Applications(client=request.user, premises=current_area, additionalInfo=request.POST['addInfo'])
+    new_app.save()
+    current_area = Premises.objects.get(pk=area_id)
+    context = {'current_area': current_area}
+    return render(request, 'rent/by_area.html', context)
